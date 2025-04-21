70e7a0e6462d44a43a8229d2027237fb95d874a7
David T. <davidt.info@gmail.com>
2025-04-21 12:49:04 -0300

    Desafio: paginação alternativa

--- src/paginas/Administracao/Restaurante/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurante/FormularioRestaurante.tsx
@@ -1,6 +1,7 @@
 import { Button, TextField } from "@mui/material";
 import axios from "axios";
 import { useState } from "react";
+import { Form } from "react-router-dom";
 
 const FormularioRestaurante = () => {
   const [nomeRestaurante, setNomeRestaurante] = useState("");
@@ -17,7 +18,7 @@ const FormularioRestaurante = () => {
   };
 
   return (
-    <form onSubmit={aoSubmeterForm}>
+    <Form onSubmit={aoSubmeterForm}>
       <TextField
         value={nomeRestaurante}
         onChange={(evento) => setNomeRestaurante(evento.target.value)}
@@ -25,7 +26,7 @@ const FormularioRestaurante = () => {
         variant="standard"
       />
       <Button type="submit" variant="outlined">Salvar</Button>
-    </form>
+    </Form>
   );
 };
 
