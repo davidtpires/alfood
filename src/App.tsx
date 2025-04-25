9413e36cf043b70699ab650519fada1bff030fa5
David T. <davidt.info@gmail.com>
2025-04-25 14:59:14 -0300

    Exibindo os pratos

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Home from './paginas/Home';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
 import AdministracaoRestaurantes from './paginas/Administracao/Restaurante/AdministracaoRestaurantes';
 import FormularioRestaurante from './paginas/Administracao/Restaurante/FormularioRestaurante';
+import PaginaBaseAdmin from './paginas/Administracao/PaginaBaseAdmin';
+import AdministracaoPratos from './paginas/Administracao/Pratos/AdministracaoPratos';
 
 function App() {
 
@@ -10,9 +12,15 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
-      <Route path="/admin/restaurantes" element={<AdministracaoRestaurantes />} />
-      <Route path="/admin/restaurantes/novo" element={<FormularioRestaurante />} />
-      <Route path="/admin/restaurantes/:id" element={<FormularioRestaurante />} />
+
+      <Route path='/admin' element={<PaginaBaseAdmin />}>
+        <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
+        <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
+        <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
+        <Route path="pratos" element={<AdministracaoPratos />} />
+
+      </Route>
+
     </Routes>
   );
 }
